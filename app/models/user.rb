7e36518f0fe615b0dc0c412fb4a14bea9aed0817
shibata07 <hsryzgzgentil49@gmail.com>
2020-10-12 08:45:20 +0000

    add devise

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  #carrierwave設定
+  mount_uploader :image, ImageUploader
+end
