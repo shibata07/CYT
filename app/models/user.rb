8824930ee30265a32f5309d3b6ed87f38313adfa
shibata07 <hsryzgzgentil49@gmail.com>
2020-10-17 03:06:49 +0000

    add follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,19 @@ class User < ApplicationRecord
 
   #carrierwave設定
   mount_uploader :image, ImageUploader
-  
+
+  validates :image, presence: true
+
   has_many :trainings, foreign_key: :user_id
+
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
+  def followed_by?(user)
+  	passive_relationships.find_by(following_id: user.id).present?
+  end
 end
+
